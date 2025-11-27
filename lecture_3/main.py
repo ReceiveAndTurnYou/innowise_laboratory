6f8cb9f86c21c6b654a2ea303eac54a1c89806c6
Ilya <93881290+ReceiveAndTurnYou@users.noreply.github.com>
2025-11-27 17:17:50 +0300

    implemented menu

--- lecture_3/main.py
+++ lecture_3/main.py
@@ -1,2 +1,23 @@
 
-students = [] #list of dictionaries 
+def display_menu(): #function that displays user menu
+    print("\t 1. Add a new student")
+    print("\t 2. Add a grades for a student")
+    print("\t 3. Show report (all students)")
+    print("\t 4. Find top performer")
+    print("\t 5. Exit")
+
+CONST_MENU_EXIT = 5
+
+students = [] #list of dictionaries
+
+choice = 0
+while choice!=CONST_MENU_EXIT:
+    display_menu()
+
+    try:
+        choice = int(input("Enter your choice: "))
+    except ValueError:
+        print("Incorrect choice!")
+        choice = 0
+
+    
